ed53f7deac8b00ba40d2d18c0b6c6051a72446ff
GraceMwende <grace.micheni@student.moringaschool.com>
2022-03-23 09:14:20 +0300

    reflect current time zone

--- faceapp/views.py
+++ faceapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import Attend
+from django.utils import timezone
+
 
 # Create your views here.
 def attend_view(request):
@@ -7,16 +9,19 @@ def attend_view(request):
   if request.method == "POST":
     if request.user.is_authenticated:
       try:
-        attended_datetime = str(Attend.objects.get(attender=request.user).datetime)[:10]
+        attended_datetime = str(timezone.now())[:10]
+        print(attended_datetime)
       except:
         pass
 
-      try:
-        attended_today = Attend.objects.filter(attender=request.user,datetime__startswith=attended_datetime)
-        status =2
-      except:
+
+      attended_today = Attend.objects.filter(attender=request.user,datetime__startswith=attended_datetime)
+      
+      if str(attended_today)[10:] == "[]>":
         status =3
 
+      else:
+        status=2
 
       if status == 3:
 
